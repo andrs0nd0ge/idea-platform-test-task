d6e406dbca33633d8a2d44830f3b964ad85309c3
Max Livingston <liv.max02@gmail.com>
2024-06-02 12:07:13 +0600

    Finished the first task, some simplifications are yet to be done

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -32,33 +32,39 @@ public class Application {
             Map<String, List<Ticket>> map = tickets.stream()
                     .collect(Collectors.groupingBy(Ticket::getCarrier));
 
-            for (Map.Entry<String, List<Ticket>> some : map.entrySet()) {
+            for (Map.Entry<String, List<Ticket>> entry : map.entrySet()) {
+                String carrier = entry.getKey();
+                List<Ticket> ticketList = entry.getValue();
 
-                List<Duration> durations = some.getValue().stream()
-                        .map(ticket -> {
-                            LocalDate arrivalDate = LocalDate.parse(ticket.getArrivalDate(), DateTimeFormatter.ofPattern("dd.MM.yy"));
-                            LocalTime arrivalTime = LocalTime.parse(ticket.getArrivalTime(), DateTimeFormatter.ofPattern("H:mm"));
-                            LocalDate departureDate = LocalDate.parse(ticket.getDepartureDate(), DateTimeFormatter.ofPattern("dd.MM.yy"));
-                            LocalTime departureTime = LocalTime.parse(ticket.getDepartureTime(), DateTimeFormatter.ofPattern("H:mm"));
+                Optional<Ticket> durationOptional = ticketList.stream()
+                        .min((t1, t2) -> {
+                            Duration d1 = getDuration(t1);
+                            Duration d2 = getDuration(t2);
 
-                            LocalDateTime arrivalDateTime = LocalDateTime.of(arrivalDate, arrivalTime);
-                            LocalDateTime departureDateTime = LocalDateTime.of(departureDate, departureTime);
-
-                            return Duration.between(departureDateTime, arrivalDateTime);
-                        })
-                        .collect(Collectors.toList());
-
-                Optional<Duration> durationOptional = durations.stream().min(Duration::compareTo);
+                            return d1.compareTo(d2);
+                        });
 
                 if (durationOptional.isPresent()) {
-                    Duration duration = durationOptional.get();
+                    Duration duration = getDuration(durationOptional.get());
 
-                    System.out.println(duration.toMinutes());
+                    System.out.println(carrier + ": " + duration.toMinutes() + " minutes");
                 }
             }
 
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+    public static Duration getDuration(Ticket ticket) {
+        LocalDate arrivalDate = LocalDate.parse(ticket.getArrivalDate(), DateTimeFormatter.ofPattern("dd.MM.yy"));
+        LocalTime arrivalTime = LocalTime.parse(ticket.getArrivalTime(), DateTimeFormatter.ofPattern("H:mm"));
+        LocalDate departureDate = LocalDate.parse(ticket.getDepartureDate(), DateTimeFormatter.ofPattern("dd.MM.yy"));
+        LocalTime departureTime = LocalTime.parse(ticket.getDepartureTime(), DateTimeFormatter.ofPattern("H:mm"));
+
+        LocalDateTime arrivalDateTime = LocalDateTime.of(arrivalDate, arrivalTime);
+        LocalDateTime departureDateTime = LocalDateTime.of(departureDate, departureTime);
+
+        return Duration.between(departureDateTime, arrivalDateTime);
     }
 }
